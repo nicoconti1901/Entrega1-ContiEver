b951197c47f8eec02d72378d3f714ac3c8f66e84
nicoconti1901 <112399532+nicoconti1901@users.noreply.github.com>
2022-09-21 12:31:56 -0300

    Add files via upload

---
+++ Proyectofinalcoder/AppFut/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from AppFut import views
+
+urlpatterns = [
+    
+    path("Inicio", views.inicio, name= "Inicio"),
+    path("socio/", views.socio, name = "Socio"),
+    path("proveedores/", views.proveedores, name = "Proveedores"),
+    path("buscarproveedor/", views.busquedaProve, name = "BuscarProve"),
+    path("buscar/",views.buscar),
+    path("turno/", views.turno, name = "Turno"),
+]
